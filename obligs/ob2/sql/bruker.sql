02cbc2f0fe8b98789ba8309c7eb3f8327023c2d9
pederDesktop <peder.wiig@gmail.com>
2017-09-27 01:36:36 +0200

    Satt opp mappe Oblig2 med struktur og tilkoblet database

---
+++ obligs/ob2/sql/bruker.sql
@@ -0,0 +1,14 @@
+DROP SCHEMA IF EXISTS Oblig2 CASCADE;
+DROP TABLE IF EXISTS Oblig2.bruker CASCADE;
+
+CREATE SCHEMA Oblig2;
+
+CREATE TABLE Oblig2.bruker (
+  telefon     VARCHAR      NOT NULL,
+  kjonn       VARCHAR,
+  fornavn     VARCHAR,
+  etternavn   VARCHAR,
+  erKasserer  BOOLEAN,
+  harBetalt   BOOLEAN,
+  PRIMARY KEY(telefon)
+);
---
+++ obligs/ob2/src/no/pederyo/model/Bruker.java
@@ -0,0 +1,7 @@
+package no.pederyo.model;
+
+/**
+ * Created by Peder on 27.09.2017.
+ */
+public class Bruker {
+}
