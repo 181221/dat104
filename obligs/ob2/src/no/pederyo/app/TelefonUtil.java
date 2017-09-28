8d1590fa2979c27ea35c9dd9e51f66f3e3368afc
pederDesktop <peder.wiig@gmail.com>
2017-09-28 22:32:26 +0200

    La til tlf formaterer

---
+++ obligs/ob2/src/no/pederyo/app/TelefonUtil.java
@@ -0,0 +1,13 @@
+package no.pederyo.app;
+
+/**
+ * Created by Peder on 28.09.2017.
+ */
+public class TelefonUtil {
+    public static String formaterTlf(String nummer) {
+        String ny = String.format("%s %s %s", nummer.substring(0,3), nummer.substring(3,5), nummer.substring(5,8));
+        return ny;
+    }
+
+
+}
